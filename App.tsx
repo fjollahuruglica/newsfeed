ea25f5ffc1497e7cd9be8fac724b1fd52c9a5400
fjollaahuruglica <fjolla@colateral.io>
2021-12-07 11:17:34 +0100

    style: add bottom navigation icons

--- App.tsx
+++ App.tsx
@@ -6,9 +6,11 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import NewsScreen from './src/containers/NewsScreen/NewsScreen';
 import SettingsScreen from './src/containers/SettingsScreen/SettingsScreen';
 import NewsDetailScreen from './src/containers/NewsDetailScreen/NewsDetailScreen';
-import { Colors } from './src/theme';
+import { Colors, Images } from './src/theme';
 import { Provider as NewsProvider } from './src/store/contexts/NewsContext';
 import { INews } from './src/store/models';
+import { Image } from 'react-native';
+import DefaultText from './src/components/DefaultText/DefaultText';
 
 const Tab = createBottomTabNavigator<RootBottomTabParamList>();
 const Stack = createStackNavigator<RootStackParamList>();
@@ -32,8 +34,42 @@ function MyTabs() {
         headerStyle: { elevation: 0 },
         headerShown: false,
       }}>
-      <Tab.Screen name="NewsScreen" component={NewsScreen} />
-      <Tab.Screen name="SettingsScreen" component={SettingsScreen} />
+      <Tab.Screen
+        name="NewsScreen"
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <DefaultText
+              text="Home"
+              style={{ color: focused ? Colors.$primary : Colors.$fontColor }}
+            />
+          ),
+          tabBarIcon: ({ size, focused }) => (
+            <Image
+              source={focused ? Images.homeActive : Images.home}
+              style={{ width: size, height: size }}
+            />
+          ),
+        }}
+        component={NewsScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <DefaultText
+              text="Settings"
+              style={{ color: focused ? Colors.$primary : Colors.$fontColor }}
+            />
+          ),
+          tabBarIcon: ({ size, focused }) => (
+            <Image
+              source={focused ? Images.settingsActive : Images.settings}
+              style={{ width: size, height: size }}
+            />
+          ),
+        }}
+        name="SettingsScreen"
+        component={SettingsScreen}
+      />
     </Tab.Navigator>
   );
 }
--- src/theme/Images.tsx
+++ src/theme/Images.tsx
@@ -1,4 +1,8 @@
 export default {
+  homeActive: require('../assets/images/home-active.png'),
+  home: require('../assets/images/home.png'),
+  settingsActive: require('../assets/images/settings-active.png'),
+  settings: require('../assets/images/settings.png'),
   search: require('../assets/images/search-icon.png'),
   time: require('../assets/images/time.png'),
   backIcon: require('../assets/images/left-arrow.png'),

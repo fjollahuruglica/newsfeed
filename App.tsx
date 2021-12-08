5a2f917c29ef0acb6e5e0a1d81076f3005d83fb1
fjollaahuruglica <fjolla@colateral.io>
2021-12-08 04:31:16 +0100

    fix: removed unneeded package

--- App.tsx
+++ App.tsx
@@ -3,22 +3,19 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as NewsProvider } from './src/store/contexts/NewsContext';
 import { Provider as ThemeProvider } from './src/store/contexts/ThemeContext';
 import { I18nextProvider } from 'react-i18next';
-import { AppearanceProvider } from 'react-native-appearance';
 import i18n from './src/i18n';
 import RootContainer from './src/RootContainer';
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <AppearanceProvider>
-        <I18nextProvider i18n={i18n}>
-          <NewsProvider>
-            <ThemeProvider>
-              <RootContainer />
-            </ThemeProvider>
-          </NewsProvider>
-        </I18nextProvider>
-      </AppearanceProvider>
+      <I18nextProvider i18n={i18n}>
+        <NewsProvider>
+          <ThemeProvider>
+            <RootContainer />
+          </ThemeProvider>
+        </NewsProvider>
+      </I18nextProvider>
     </SafeAreaProvider>
   );
 };
